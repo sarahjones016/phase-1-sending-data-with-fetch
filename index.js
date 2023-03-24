85c67734372b0b12b31786d976aad5ac8f5ffaf3
Sarah Jones <sarahrjones8@gmail.com>
2023-03-24 15:40:00 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+function submitData(userName, userEmail) {
+
+    const formData = {
+        name: userName,
+        email: userEmail,
+    };
+    
+    const configObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+          },
+          body: JSON.stringify(formData),
+    };
+
+    return fetch("http://localhost:3000/users", configObject)
+    .then(function (res) {
+        return res.json();
+    })
+    .then(function (object) {
+        document.body.textContent = object.id
+    })
+    .catch(function (error) {
+        alert("See page for more details on the alert");
+        document.body.textContent = error.message;
+    })
+};
+
